59a4498d29bbfc1abdd2bebde53ef9f3ab4394b8
BuildTools <unconfigured@null.spigotmc.org>
2020-10-16 19:29:08 +0530

    Pwa stuff

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import VueRouter from 'vue-router'
 import Bar from './components/chartComponets/Bar.vue'
 import Pie from './components/chartComponets/Pie.vue'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
